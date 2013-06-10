d12f527b6cbdfebc158a7a16217e89cabab5b4de
liquid0624 <rjbirnesser@gmail.com>
2013-06-10 15:17:44 -0400

    Revert "htc: lte: updated driver"
    This reverts commit d0a44aceb173282000d0d3c90c9669a6bd8b5ec0.

--- drivers/net/7x30-lte/msm_rmnet_sdio.h
+++ drivers/net/7x30-lte/msm_rmnet_sdio.h
@@ -25,11 +25,19 @@
  * OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
  * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-#if defined(CONFIG_ARCH_MSM7X30_LTE)
-#include <mach/7x30-lte/peripheral-loader.h>
-#endif
 
-#ifndef __MACH_PERIPHERAL_LOADER_H
-#define __MACH_PERIPHERAL_LOADER_H
+#include <linux/types.h>
+#include <linux/skbuff.h>
 
-#endif
+#ifndef _MSM_RMNET_SDIO_H
+#define _MSM_RMNET_SDIO_H
+
+int msm_rmnet_sdio_open(uint32_t id, void *priv,
+			void (*receive_cb)(void *, struct sk_buff *),
+			void (*write_done)(void *, struct sk_buff *));
+
+int msm_rmnet_sdio_close(uint32_t id);
+
+int msm_rmnet_sdio_write(uint32_t id, struct sk_buff *skb);
+
+#endif /* _MSM_RMNET_SDIO_H */
